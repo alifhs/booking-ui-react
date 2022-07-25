f4028adc80eaabd12cdc69fd12e2c05f97f5bab9
alifhs <alif.hs@outlook.com>
2022-07-25 23:53:40 +0600

    add featured places

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -10,7 +10,7 @@ export const Home = () => {
         <Header />
         <div className="homeContainer">
           <Feature />
-          <Feature />
+          <h1 className='homeTitle'>Browse by property type</h1>
         </div>
     </div>
   )
