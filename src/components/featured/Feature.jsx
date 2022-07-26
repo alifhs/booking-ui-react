30917df67dafdcac022aced1f319585a3201b9af
alifhs <alif.hs@outlook.com>
2022-07-26 20:59:19 +0600

    property list added

--- src/components/featured/Feature.jsx
+++ src/components/featured/Feature.jsx
@@ -8,7 +8,7 @@ export const Feature = () => {
           <img src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o=" className='featuredImg' alt="" />
           <div className="featuredTitles">
               <h1 className='featurePlaceName'>Dublin</h1>
-              <h2 className='featurePlaceDetails'>Ab properties</h2>
+              <h2 className='featurePlaceDetails'>231 properties</h2>
           </div>
         </div>
         <div className="featuredItem">
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
 import { Feature } from '../../components/featured/Feature';
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties';
 import { Header } from '../../components/header/Header';
 import { Navbar } from '../../components/navbar/Navbar';
+import { PropertyList } from '../../components/propertyList/propertyList';
 import './home.css';
 
 export const Home = () => {
@@ -11,6 +13,8 @@ export const Home = () => {
         <div className="homeContainer">
           <Feature />
           <h1 className='homeTitle'>Browse by property type</h1>
+          <PropertyList />
+          <FeaturedProperties />
         </div>
     </div>
   )
